560b8a267e5ad40bd06deecf7899ce3fdc397d11
RiN-NiK <RiN-NiK@users.noreply.github.com>
2026-05-19 19:37:40 +0300

    Добавлена возможность одновременно подключаться к мониторинг серверу нескольким клиентам

--- Models/MainServer.cs
+++ Models/MainServer.cs
@@ -1,18 +1,14 @@
-﻿namespace MonitoringServer.Models
+﻿using System.Collections.Concurrent;
+
+namespace MonitoringServer.Models
 {
     internal class MainServer
     {
-        public Dictionary<int, Server> Servers { get; } = new(); // достаточно обычного dictionary, т.к. нет необходимости в долгом сеансе
-                                                                 // из-за специфики приложения нет необходимости использования асинхронности при подключении клиентов                                                       
+        public ConcurrentDictionary<int, Server> Servers { get; } = new();                                                       
         public void addServer(int id, Server server)
         {
-            Servers.Add(id, server);
+            Servers.TryAdd(id, server);
         }
-        public void removeServer(int id)
-        {
-            Servers.Remove(id);
-        }
-
     }
     
 }
--- Program.cs
+++ Program.cs
@@ -37,63 +37,7 @@ namespace MonitoringServer
                 while (true)
                 {
                     TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync(); // мониторинг сервер ожидает подключения клиента
-
-                    try
-                    {
-                        Console.WriteLine($"Подключился клиент: ({DateTime.UtcNow}) {tcpClient.Client.RemoteEndPoint}");
-
-                        StringBuilder message = new StringBuilder(); // используется для формирования ответа сервера
-                        bool isStopped = false; // переменная, которая позволяет клиенту отключиться 
-
-                        using (NetworkStream networkStream = tcpClient.GetStream()) // установка соединения клиента с мониторинг сервером для обмена байтами
-                        {
-                            while (!isStopped)
-                            {
-                                byte[] buffer = new byte[1024];
-
-                                int bytesRead = await networkStream.ReadAsync(buffer, 0, buffer.Length);
-
-                                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead); // расшифровываем массив байт от клиента
-
-                                switch (receivedMessage) // ядро программы (функционал сервера): формируется ответ сервера на запрос, который записывается в stringBuilder
-                                {
-                                    case "Show information about all servers": // вывод полной информации о всех серверах
-
-                                        message.AppendLine($"{DateTime.UtcNow}{Environment.NewLine}");
-
-                                        foreach (var server in mainServer.Servers.Values)
-                                        {
-                                           message.AppendLine($"ID сервера: {server.ServerId}{Environment.NewLine}" + // Environment.NewLine позволяет корректно отобразить переход на новую строку
-                                                               $"температура CPU - {server.CPUtemperature} °C{Environment.NewLine}" +
-                                                               $"температура GPU - {server.GPUtemperature} °C{Environment.NewLine}" +
-                                                               $"тактовая частота CPU - {server.CPUclockSpeed} GHz{Environment.NewLine}" +
-                                                               $"рабочее состояние сервера - {server.IsEnabled}{Environment.NewLine}" +
-                                                               $"-----------------------------------------{Environment.NewLine}");
-                                        }
-                                            break;
-                                 
-                                    case "Exit": // завершить соединение клиента с мониторинг сервером (следующий клиент может подключиться)
-                                        isStopped = true;
-                                        message.AppendLine("Приложение отключено.");
-                                        break;
-                                }
-
-                                byte[] responseByte = Encoding.UTF8.GetBytes(message.ToString()); // преобразование ответа сервера в массив байт с дальнейшей отправкой
-                                await networkStream.WriteAsync(responseByte);
-                                message.Clear(); // зачищаем переменную, чтобы данные не перемешались с предыдущими
-                            }
-
-                            Console.WriteLine($"Клиент был отключен: ({DateTime.UtcNow}) {tcpClient.Client.RemoteEndPoint}");
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Произошла ошибка: ({DateTime.UtcNow}) {ex.Message}");
-                    }
-                    finally
-                    {
-                        tcpClient?.Close();
-                    }
+                    _ = Task.Run(async () => ClientHandler(tcpClient, mainServer)); 
                 }
             }
             catch (Exception ex)
@@ -103,9 +47,67 @@ namespace MonitoringServer
             finally
             {
                 tcpListener.Stop();
-                Console.WriteLine($"Сервер закончил работу ({DateTime.UtcNow}) {ipAddress.ToString()}:{port}");
+                Console.WriteLine($"Сервер завершил работу ({DateTime.UtcNow}) {ipAddress.ToString()}:{port}");
             }
+        }
+        static async Task ClientHandler(TcpClient tcpClient, MainServer mainServer)
+        {
+            try
+            {
+                Console.WriteLine($"Подключился клиент: ({DateTime.UtcNow}) {tcpClient.Client.RemoteEndPoint}");
 
+                StringBuilder message = new StringBuilder(); // используется для формирования ответа сервера
+                bool isStopped = false; // переменная, которая позволяет клиенту отключиться 
+
+                using (NetworkStream networkStream = tcpClient.GetStream()) // установка соединения клиента с мониторинг сервером для обмена байтами
+                {
+                    while (!isStopped)
+                    {
+                        byte[] buffer = new byte[1024];
+
+                        int bytesRead = await networkStream.ReadAsync(buffer, 0, buffer.Length);
+
+                        string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead); // расшифровываем массив байт от клиента
+
+                        switch (receivedMessage) // ядро программы (функционал сервера): формируется ответ сервера на запрос, который записывается в stringBuilder
+                        {
+                            case "Show information about all servers": // вывод полной информации о всех серверах
+
+                                message.AppendLine($"{DateTime.UtcNow}{Environment.NewLine}");
+
+                                foreach (var server in mainServer.Servers.Values)
+                                {
+                                    message.AppendLine($"ID сервера: {server.ServerId}{Environment.NewLine}" + // Environment.NewLine позволяет корректно отобразить переход на новую строку
+                                                        $"температура CPU - {server.CPUtemperature} °C{Environment.NewLine}" +
+                                                        $"температура GPU - {server.GPUtemperature} °C{Environment.NewLine}" +
+                                                        $"тактовая частота CPU - {server.CPUclockSpeed} GHz{Environment.NewLine}" +
+                                                        $"рабочее состояние сервера - {server.IsEnabled}{Environment.NewLine}" +
+                                                        $"-----------------------------------------{Environment.NewLine}");
+                                }
+                                break;
+
+                            case "Exit": // завершить соединение клиента с мониторинг сервером (следующий клиент может подключиться)
+                                isStopped = true;
+                                message.AppendLine("Приложение отключено.");
+                                break;
+                        }
+
+                        byte[] responseByte = Encoding.UTF8.GetBytes(message.ToString()); // преобразование ответа сервера в массив байт с дальнейшей отправкой
+                        await networkStream.WriteAsync(responseByte);
+                        message.Clear(); // зачищаем переменную, чтобы данные не перемешались с предыдущими
+                    }
+
+                    Console.WriteLine($"Клиент был отключен: ({DateTime.UtcNow}) {tcpClient.Client.RemoteEndPoint}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: ({DateTime.UtcNow}) {ex.Message}");
+            }
+            finally
+            {
+                tcpClient?.Close();
+            }
         }
     }
 }
